818fdc408d6544756e14086eef82cfe0450c8426
Hardik Khandal <hardikkhandal@gmail.com>
2024-07-16 23:23:38 +0530

    Helo ji

--- api/summarize.ts
+++ api/summarize.ts
@@ -1,5 +1,5 @@
     import type { VercelRequest, VercelResponse } from '@vercel/node'
-    const express = require("express");
+    import express from 'express';
     const { getTranscript } = require("youtube-transcript-api");
     import { YoutubeTranscript } from 'youtube-transcript';
     const { generateText } = require("../services/groqServices");
@@ -7,20 +7,19 @@
 
     const router = express.Router();
 
-    export default function handler(req: VercelRequest, res: VercelResponse) {
+    export default async function handler(req: VercelRequest, res: VercelResponse) {
     try {
         console.log("Request body:", req.body);
         const { videoUrl } = req.body;
         console.log("Request received to summarize video:", videoUrl);
         const videoId = extractVideoId(videoUrl);
 
-        const transcript = getTranscript(videoId, { timeout: 60000 });
-        const transcriptText = transcript.map((entry) => entry.text).join(" ");
+        const transcript = await getTranscript(videoId, { timeout: 60000 });
+        const transcriptText = transcript.map((entry: any) => entry.text).join(" ");
         console.log("Fetched transcript:", transcriptText);
 
         const prompt = `Summarize the video at the following URL: ${videoUrl}. Transcript: ${transcriptText} in few words`;
-        // const summary = generateText("llama3-8b-8192", prompt);
-        const summary = "Helo ji";
+    const summary = await generateText("llama3-8b-8192", prompt);
 
         console.log(summary);
         res.json({ summary: summary});
