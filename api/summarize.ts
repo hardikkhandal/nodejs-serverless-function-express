46a63f1120e93c60e70d0f113134fa2b475a15be
Hardik Khandal <hardikkhandal@gmail.com>
2024-07-16 20:31:49 +0530

    changed some things

--- api/summarize.ts
+++ api/summarize.ts
@@ -1,28 +1,29 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-const express = require("express");
-const { getTranscript } = require("youtube-transcript-api");
-const { generateText } = require("../services/groqServices");
-const { extractVideoId } = require("../services/extractVideoId");
+    import type { VercelRequest, VercelResponse } from '@vercel/node'
+    const express = require("express");
+    const { getTranscript } = require("youtube-transcript-api");
+    import { YoutubeTranscript } from 'youtube-transcript';
+    const { generateText } = require("../services/groqServices");
+    const { extractVideoId } = require("../services/extractVideoId");
 
-const router = express.Router();
+    const router = express.Router();
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  try {
-    // console.log("Request body:", req.body);
-    // const { videoUrl } = req.body;
-    // console.log("Request received to summarize video:", videoUrl);
-    // const videoId = extractVideoId(videoUrl);
+    export default function handler(req: VercelRequest, res: VercelResponse) {
+    try {
+        console.log("Request body:", req.body);
+        const { videoUrl } = req.body;
+        console.log("Request received to summarize video:", videoUrl);
+        const videoId = extractVideoId(videoUrl);
 
-    // const transcript = getTranscript(videoId, { timeout: 60000 });
-    // const transcriptText = transcript.map((entry) => entry.text).join(" ");
-    // console.log("Fetched transcript:", transcriptText);
+        // const transcript = getTranscript(videoId, { timeout: 60000 });
+        let transcriptText = YoutubeTranscript.fetchTranscript('videoId or URL').then(console.log);
+        console.log("Fetched transcript:", transcriptText);
 
-    // const prompt = `Summarize the video at the following URL: ${videoUrl}. Transcript: ${transcriptText} in few words`;
-    // const summary = generateText("llama3-8b-8192", prompt);
+        const prompt = `Summarize the video at the following URL: ${videoUrl}. Transcript: ${transcriptText} in few words`;
+        const summary = generateText("llama3-8b-8192", prompt);
 
-    res.json({ summary: "good morning" });
-  } catch (error) {
-    console.error("Error summarizing video:", error.message);
-    res.status(500).json({ error: "Failed to summarize video" });
-  }
-}
+        res.json({ summary: "good morning" });
+    } catch (error) {
+        console.error("Error summarizing video:", error.message);
+        res.status(500).json({ error: "Failed to summarize video" });
+    }
+    }
