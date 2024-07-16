325b6cd418bce1038db2048166dbe5d275c359cf
Hardik Khandal <hardikkhandal@gmail.com>
2024-07-17 00:31:11 +0530

    Helo jiis

--- api/summarize.ts
+++ api/summarize.ts
@@ -8,6 +8,9 @@
     const router = express.Router();
 
     export default async function handler(req: VercelRequest, res: VercelResponse) {
+        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     try {
         console.log("Request body:", req.body);
         console.log("Req body:", req.body);
